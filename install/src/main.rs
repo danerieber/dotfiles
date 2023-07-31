68f059c9212318231cc55ba82f14e3648c2303b9
Dane Rieber <danerieber@gmail.com>
2023-07-31 14:38:55 -0500

    Find all files in root dir and print relative path

--- install/src/main.rs
+++ install/src/main.rs
@@ -1,3 +1,37 @@
+use std::path::{Path, PathBuf};
+
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+struct Args {
+    root: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let root_dir = args.root.clone();
+
+    for entry in WalkDir::new(args.root) {
+        let root = root_dir.clone();
+
+        let entry = entry.expect("Error while iterating over directory entries");
+        let file_path = entry.path();
+
+        if file_path.is_file() {
+            let file_path_buf = PathBuf::from(file_path);
+
+            let relative_path = get_relative_path(&root, &file_path_buf);
+
+            println!("Relative path: {}", relative_path.to_str().unwrap());
+        }
+    }
+}
+
+fn get_relative_path(root_dir: &Path, file_path: &Path) -> PathBuf {
+    if let Ok(relative_path) = file_path.strip_prefix(root_dir) {
+        PathBuf::from(relative_path)
+    } else {
+        file_path.to_path_buf()
+    }
 }
