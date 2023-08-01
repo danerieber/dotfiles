d57b6a7d4341b2a3b6e6b837519c45458e298f37
Dane Rieber <danerieber@gmail.com>
2023-07-31 22:02:34 -0500

    Get target file path and ask user to replace

--- install/src/main.rs
+++ install/src/main.rs
@@ -1,6 +1,11 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
+use dialoguer::Confirm;
+use dirs::home_dir;
 use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
@@ -13,17 +18,43 @@ fn main() {
     let root_dir = args.root.clone();
 
     for entry in WalkDir::new(args.root) {
-        let root = root_dir.clone();
+        let root_dir = root_dir.as_path();
 
         let entry = entry.expect("Error while iterating over directory entries");
         let file_path = entry.path();
 
         if file_path.is_file() {
             let file_path_buf = PathBuf::from(file_path);
+            let file_path = file_path_buf.as_path();
 
-            let relative_path = get_relative_path(&root, &file_path_buf);
+            let relative_path_buf = get_relative_path(root_dir, file_path);
+            let relative_path = relative_path_buf.to_str().unwrap();
+            let actual_path = evaluate_path(relative_path);
+            let backup_path = format!("{}{}", actual_path, ".bak");
 
-            println!("Relative path: {}", relative_path.to_str().unwrap());
+            println!("Relative path: {}", &relative_path);
+            println!("Actual path: {}", actual_path);
+            println!("Backup path: {}", backup_path);
+
+            if file_exists(&actual_path) {
+                print!("File exists");
+                if file_exists(&backup_path) {
+                    print!(" AND .bak File exsits");
+                }
+                println!();
+
+                let should_replace = Confirm::new()
+                    .default(true)
+                    .with_prompt("Replace file anyways?")
+                    .wait_for_newline(true)
+                    .interact()
+                    .unwrap();
+                if should_replace {
+                    println!("REPLACE FILE");
+                } else {
+                    println!("DONT REPLACE FILE");
+                }
+            }
         }
     }
 }
@@ -33,5 +64,27 @@ fn get_relative_path(root_dir: &Path, file_path: &Path) -> PathBuf {
         PathBuf::from(relative_path)
     } else {
         file_path.to_path_buf()
+    }
+}
+
+fn evaluate_path(path: &str) -> String {
+    let expanded_path = if path.starts_with('~') {
+        if let Some(home) = home_dir() {
+            path.replacen('~', home.to_str().unwrap(), 1)
+        } else {
+            path.to_string()
+        }
+    } else {
+        path.to_string()
+    };
+
+    expanded_path
+}
+
+fn file_exists(file_path: &str) -> bool {
+    if let Ok(metadata) = fs::metadata(file_path) {
+        metadata.is_file()
+    } else {
+        false
     }
 }
